ca0b4c7994dd24b114f937d9092a25d3273aaf4b
tejasvi8686 <tejasviraj8686@gmail.com>
2023-07-19 23:21:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./normal.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="avatar">Me</div>
+          </div>
+          <div className="message">Hello world</div>
+        </div>
+
+        <div className="chat-input-holder">
+          <textarea row="1" className="chat-input-textarea"></textarea>
+        </div>
+      </section>
     </div>
   );
 }
 
 export default App;
+
+
+
